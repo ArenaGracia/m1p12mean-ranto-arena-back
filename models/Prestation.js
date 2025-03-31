512d27155dc18b491dde14db5eee68015c0aa1f0
ArenaGracia <gracia.rabeseranana@gmail.com>
2025-03-31 09:01:35 +0300

    ajouter view

--- models/Prestation.js
+++ models/Prestation.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const CategorySchema = new mongoose.Schema({
     name: { type: String, required: true }, 
+    prestations: [{ type: mongoose.Schema.Types.ObjectId, ref: 'prestation' }]
 }, { collection: 'category' });
 
 const PrestationSchema = new mongoose.Schema({
---
+++ server-dev.js
@@ -0,0 +1,46 @@
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const cors = require('cors'); 
+require('dotenv').config(); 
+const bodyParser = require('body-parser');
+const authMiddleware = require('./middlewares/authMiddleware');
+const app = express(); 
+const PORT = process.env.PORT || 5000; 
+
+const whitelist = ['http://localhost:4200']; // sites autorisés
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log(origin);
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // autoriser les cookies / tokens
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Middleware 
+app.use(cors(corsOptions)); 
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(authMiddleware);
+
+// Connexion à MongoDB 
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(() => console.log("MongoDB connecté")) 
+  .catch(err => console.log(err)); 
+
+// Routes
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/api/quotes', require('./routes/quoteRoute'));
+app.use('/api/prestations', require('./routes/prestationRoute'));
+app.use('/api/categories', require('./routes/categoryRoute'));
+app.use('/api/email', require('./routes/emailRoute'));
+
+app.listen(PORT, () => console.log(`Serveur démarré sur le port 
+    ${PORT}`)); 
